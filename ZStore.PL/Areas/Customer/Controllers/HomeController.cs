7a5fa5f429e6d8332c7e4860bbefdaedaef790cd
Ahmed <Ahmed@DESKTOP-6PQH1K9>
2024-05-09 19:48:57 +0300

    Home and Details in action with views for customers

--- ZStore.PL/Areas/Customer/Controllers/HomeController.cs
+++ ZStore.PL/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ZStore.Core;
 using System.Diagnostics;
+using ZStore.Application.Repository.IRepository;
 
 namespace Store.PL.Areas.Customer.Controllers
 {
@@ -8,15 +9,23 @@ namespace Store.PL.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork _unitOfWork)
         {
             _logger = logger;
+            unitOfWork = _unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = unitOfWork.Product.Get(u=>u.Id==id,includeProperties:"Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
