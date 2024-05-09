80b5dd4569af1cd94744b45d436b6a60203f4ffe
Ahmed <Ahmed@DESKTOP-6PQH1K9>
2024-05-09 20:01:29 +0300

    Scaffolding Identity module to use it in Project

--- ZStore.Data/ApplicationDbContext.cs
+++ ZStore.Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ZStore.Core;
 
 namespace ZStore.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext :IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -15,6 +16,7 @@ namespace ZStore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
@@ -106,7 +108,7 @@ namespace ZStore.Data
                     ImageUrl = ""
                 }
                 );
-            base.OnModelCreating(modelBuilder);
+           
         }
     }
 }
--- ZStore.PL/Program.cs
+++ ZStore.PL/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ZStore.Application.Repository.IRepository;
 using ZStore.Application.Repository;
 using ZStore.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Cs"));
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
